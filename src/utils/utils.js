5124a5ca0ba3d29936577d2f9207163f1db2e5a0
camronh <camronhaider@gmail.com>
2021-07-11 23:29:03 -0400

    hotfix
    Added fixedparams to configs

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -225,6 +225,8 @@ function makeConfig(state) {
           fixed: endpoint.reservedParam.path,
         },
       ],
+      fixedOperationParameters: [],
+
       parameters: endpoint.params.map(param => {
         return {
           name: param.name,
