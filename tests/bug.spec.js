f3f3c28006520a663580b0e20d642ac9930257ff
camronh <camronhaider@gmail.com>
2021-09-02 19:19:37 -0400

    Bug fix: Bulk add param failed on second time

--- tests/bug.spec.js
+++ tests/bug.spec.js
@@ -23,4 +23,28 @@ test.describe("Bug Fix", () => {
     expect(await page.isVisible("text=test")).toBeFalsy();
     //   await page.click(
   });
+
+  test("Bug Fix: Bulk Add Param fails the second time", async ({ page }) => {
+    await page.goto("http://localhost:8080/");
+    await page.click("#menuButton");
+
+    await page.click("text=Import");
+    const exampleConfig = require("../src/utils/exampleConfig.json");
+    await page.fill("textarea", JSON.stringify(exampleConfig));
+    await page.click("[type='submit']");
+
+    await page.click("#menuButton");
+    await page.click("text=Bulk");
+    await page.click("text=Add Param");
+    await page.keyboard.type("Test1");
+    await page.click("text=Bulk Add Param");
+    expect(await page.isVisible("text=Test1 - query")).toBeTruthy();
+    await page.click("text=Add Param");
+    await page.type("text=Param Name", "Test2");
+    await page.click("text=Bulk Add Param");
+    expect(await page.isVisible("text=Test1 - query")).toBeTruthy();
+    expect(await page.isVisible("text=Test2 - query")).toBeTruthy();
+
+    //   await page.click(
+  });
 });
